24be3caaa59c5a10bce888d909d6dbe8b56e0fb4
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-09-08 16:57:59 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,15 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
+def square(x):
+  t.forward(x)
+  t.left(90)
+  t.forward(x)
+  t.left(90)
+  t.forward(x)
+  t.left(90)
+  t.forward(x)
+  t.left(90)
+square(200)
 
 turtle.done()
