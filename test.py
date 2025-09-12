4e66545f686b7a05b2f9a0f862148f95cc998bff
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-09-12 07:32:26 -0700

    commit

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import turtle
+""" import turtle
 from turtle import *
 t = Turtle()
 
@@ -7,6 +7,9 @@ t.shape('turtle')
 
 t.speed(0)
 
+turtle.done()
+ """
+# UNIT ONE
 """ def square(x):
     length = 5
     for i in range(60):
@@ -31,14 +34,75 @@ t.speed(0)
             t.left(90)
 square(4)  """
 
-def star(x)
-length = 5
-for i in range(60):
-    t.left(5)
-    length += 5
-    for i in range(x):
-        t.forward(length)
-        t.left(144)
-star(5)
+""" def star(x)
 
-turtle.done()
+ x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) 
+
+print(values[0])
+print(values[6])
+ """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+
+# return is output
+
+def add(x,y):
+    return(x + y)
+bill = add(5,10)
+print(bill)
+
+z = 15
+bill = input("whats the bill amount")
+print(add(15, int(bill))) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+ """
+
+""" temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+values = [3]
+number = 3
+if number == (values):
+    print("odd")
+else:
+    print("even")
+
+def add(x, y):
+    return(x + y)
+bill = add(10,5)
+
+z = 15
+def service(bad):
+    if service == (bad):
+        return("0% tip")
+   """  if service == (okay):
+        return("10% tip")
+    if service == (good):
+        return("15% tip")
+    if service == (great)
+        return("25% tip") """
+bill = input("how much is the bill?")
+print(add(15, int(z)))
+print("how was the service?")
+print("bad")
